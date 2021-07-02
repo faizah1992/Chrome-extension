0584a4031654e3fca46574090f712371663cd985
Faizah Ahsan <faizah1992@gmail.com>
2021-07-01 23:13:51 -0500

    js file updated

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-let inputBtn = document.getElementById("input-btn")
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+console.log(ulEl)
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked!")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
+
+
+for (let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i])
+}
